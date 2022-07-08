ab4f3df324b56685ab545db6d7533e08f6d8f6c7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-07-08 17:43:14 +0100

    Added teams generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,62 @@ export class AppComponent {
   newMemberName = ``;
   members: string[] = [];
   errorMessage = ``;
+  numberOfTeams: number | ""= "";
+  teams: string[][] = [];
 
   onInput(member: string) {
     this.newMemberName = member;
-    
+      }
 
-    
+
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams = Number(value);
       }
 
   addMember() {
 
     if(!this.newMemberName) {
-      this.errorMessage = "Name can't be empty";
+      this.errorMessage = "Name can't be empty 😥";
       return ;
     }
     this.errorMessage = "";
     this.members.push(this.newMemberName);
     this.newMemberName = ``;
    
+  }
+
+  generateTeams() {
+    if(!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = "Invalid number of teams ☹️";
+      return
+    }
+     
+    if(this.members.length < this.numberOfTeams) {
+      this.errorMessage = "Not enough members ☹️";
+    }
+
+    this.errorMessage = "";
+    const allMembers = [...this.members];
+
+    while(allMembers.length) {
+      for(let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0];
+        
+        if(!member)break;
+
+
+        if (this.teams[i]) {
+          this.teams[i].push(member)
+        } else {
+          this.teams[i] = [member]
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = "";
+    
   }
 
 
